094d7b136e86b09a581d4b67c79cee6c46ace127
CODirov <nodirbek29@gmail.com>
2023-05-01 12:51:57 +0500

    profile done

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import login_view, register_view
+from .views import login_view, register_view, profile_view
 
 app_name = "users"
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
+    path('profile/', profile_view, name='profile'),
 ]
