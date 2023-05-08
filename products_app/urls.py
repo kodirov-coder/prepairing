30c0d935488bd4d6f5e45cc77a9d5a2623ab2ee2
CODirov <nodirbek29@gmail.com>
2023-05-08 17:37:46 +0500

    order by category

--- products_app/urls.py
+++ products_app/urls.py
@@ -7,6 +7,7 @@ app_name = "products"
 urlpatterns = [
     path("", index, name="main-page"),
     path("products", products, name="products"),
+    path("category/<int:category_id>/", products, name="category"),
     path("basket/add/<int:product_id>/", basket_add, name="basket-add"),
     path("basket/remove/<int:basket_id>/", basket_remove, name="basket-remove"),
 ]
