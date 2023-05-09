c8dbcd510efb4ce8a258a4f5c4135e17596b5f2d
CODirov <nodirbek29@gmail.com>
2023-05-09 17:56:29 +0500

    class IndexView

--- store_pr/urls.py
+++ store_pr/urls.py
@@ -3,9 +3,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from products_app.views import IndexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("products_app.urls")),
+    path("", IndexView.as_view(), name="main-page"),
+    path('products', include("products_app.urls")),
     path('users/', include("users_app.urls"))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
